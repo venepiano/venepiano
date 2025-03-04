b6e0675bc95991c3b6ff581cd90a129f37734776
gosekose <ksyn1611@naver.com>
2025-03-04 14:01:48 +0900

    [VENEPIANO-001] feat: 멀티모듈 초기 세팅

---
+++ module-order/src/main/java/com/venepiano/ModuleOrderApplication.java
@@ -0,0 +1,13 @@
+package com.venepiano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleOrderApplication.class, args);
+    }
+
+}
---
+++ module-order/src/test/java/com/venepiano/ModuleOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venepiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
