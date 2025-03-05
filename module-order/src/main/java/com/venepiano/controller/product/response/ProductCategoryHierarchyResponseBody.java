16793f43fe136e79b84236e6f70a5cfe5be762b3
gosekose <ksyn1611@naver.com>
2025-03-06 00:34:11 +0900

    reactor(#12): 상품 카테고리 계층별 조회 줄바꿈 추가

--- module-order/src/main/java/com/venepiano/controller/product/response/ProductCategoryHierarchyResponseBody.java
+++ module-order/src/main/java/com/venepiano/controller/product/response/ProductCategoryHierarchyResponseBody.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductCategoryHierarchyResponseBody {
+
     private ProductCategoryField category;
     private List<ProductCategoryHierarchyResponseBody> subCategories;
 
@@ -50,4 +51,5 @@ public class ProductCategoryHierarchyResponseBody {
                 .subCategories(convertedSubCategories)
                 .build();
     }
+
 }
